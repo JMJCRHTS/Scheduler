87643939276e2a3485df58f5c18a3ebfd5c8079e
Noor Chasib <noorchasib@gmail.com>
2023-01-14 14:47:40 -0800

    implemented axios, helper components and test

---
+++ src/helpers/selectors.js
@@ -0,0 +1,12 @@
+export function getAppointmentsForDay(state, day) {
+  const dayAppointments = [];
+  const dayApptIds =
+    state.days.find((el) => el.name === day)?.appointments || undefined;
+
+  dayApptIds &&
+    dayApptIds.forEach((id) => {
+      dayAppointments.push(state.appointments[id]);
+    });
+
+  return dayAppointments;
+}
